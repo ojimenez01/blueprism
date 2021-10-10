f7446f202fdb7e30371a8e394ebd5d080951a20c
Oscar Jimenez <oscar.jimenez@wu.com>
2021-10-10 17:28:10 -0600

    commit 8

--- export release/releaseexport.bat
+++ export release/releaseexport.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /exportpackage %1 /user %2 %3 /dbconname %4
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /exportpackage "ACE Package 2" /user ojimenez 3Mkmk0501! /dbconname PreProd/UAT
