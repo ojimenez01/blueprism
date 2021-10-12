70ff0ad06ec26371722495225a3d631288fe03c3
Oscar Jimenez <oscar.jimenez@wu.com>
2021-10-11 19:52:18 -0600

    commit 57

--- release 1/processes/processdeploy.bat
+++ release 1/processes/processdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "Test.xml" /overwrite /user %1 %2 /dbconname %3
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "0728-01.xml" /overwrite /user "ojimenez" "Western123!" /dbconname Dev/SandBox
