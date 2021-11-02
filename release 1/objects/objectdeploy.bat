8e2433fc7c37d5fa34535055432f39f3525df0a1
Oscar Jimenez <oscar.jimenez@wu.com>
2021-11-02 08:49:31 -0600

    commit 11/1

--- release 1/objects/objectdeploy.bat
+++ release 1/objects/objectdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Object - spy test.bpobject" /overwrite /user "ojimenez" "Western123!" /dbconname Dev/SandBox
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Object - spy test.bpobject" /overwrite /user %1 %2 /dbconname %3
