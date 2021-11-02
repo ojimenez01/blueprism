3fc5619afcd9dff1a04c1674c3686649f7c643cc
Oscar Jimenez <oscar.jimenez@wu.com>
2021-11-02 09:32:04 -0600

    commit 11/1 9:31

--- release 1/processes/processdeploy.bat
+++ release 1/processes/processdeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Process - 0728-01.bpprocess" /overwrite /user "ojimenez" "Western123!" /dbconname Dev/SandBox
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Process - 0728-01.bpprocess" /overwrite /user %1 %2 /dbconname %3
--- release 1/releases/releasedeploy.bat
+++ release 1/releases/releasedeploy.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease "releaseGit001.bprelease" /overwrite /user ojimenez Western123! /dbconname Dev/SandBox
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease "releaseGit001.bprelease" /overwrite /user %1 %2 /dbconname %3
